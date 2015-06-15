8b458dc331daa7769625ad07fb17fb5e487b5458
Helen Yang <helen@hyyang.local>
2015-06-14 23:16:16 -0400

    Implement IBActions

--- iCalculator/ViewController.h
+++ iCalculator/ViewController.h
@@ -8,8 +8,46 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+typedef NS_ENUM(NSInteger, Operation) {
+    Add,
+    Substract,
+    Multiply,
+    Divide,
+    None
+};
 
+@interface ViewController : UIViewController {
+    Operation _operation;
+    double _display;
+    double _result;
+    BOOL _isDecimal;
+}
+
+
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+
+- (IBAction)ac:(id)sender;
+- (IBAction)plusMinus:(id)sender;
+- (IBAction)divide:(id)sender;
+- (IBAction)multiply:(id)sender;
+- (IBAction)subsctract:(id)sender;
+- (IBAction)add:(id)sender;
+- (IBAction)equal:(id)sender;
+- (IBAction)dot:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)zero:(id)sender;
+
+- (void)setResultWithNumber:(int)number;
+- (void)numberWithOperation:(Operation)operation;
 
 @end
 
--- iCalculator/ViewController.m
+++ iCalculator/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _isDecimal = false;
+    _result = 0;
+    self.resultLabel.adjustsFontSizeToFitWidth = TRUE;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +28,159 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ac:(id)sender {
+    _result = 0;
+    _display = 0;
+    _isDecimal = false;
+    self.resultLabel.text = @"0";
+}
+
+- (IBAction)plusMinus:(id)sender {
+    _display = _display * (-1);
+    if (_display < 0) {
+        self.resultLabel.text = [@"-" stringByAppendingString:self.resultLabel.text];
+    } else {
+        if ([self.resultLabel.text hasPrefix:@"-"]) {
+            self.resultLabel.text = [self.resultLabel.text substringFromIndex:1];
+        }
+    }
+}
+
+- (IBAction)divide:(id)sender {
+    if (_result != 0) {
+        [self numberWithOperation:_operation];
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", _result];
+        _display = [self.resultLabel.text floatValue];
+        _result = 0;
+    }
+    
+    [self numberWithOperation:Divide];
+}
+
+- (IBAction)multiply:(id)sender {
+    if (_result != 0) {
+        [self numberWithOperation:_operation];
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", _result];
+        _display = [self.resultLabel.text floatValue];
+        _result = 0;
+    }
+    
+    [self numberWithOperation:Multiply];}
+
+- (IBAction)subsctract:(id)sender {
+    if (_result != 0) {
+        [self numberWithOperation:_operation];
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", _result];
+        _display = [self.resultLabel.text floatValue];
+        _result = 0;
+    }
+    
+    [self numberWithOperation:Substract];
+}
+
+- (IBAction)add:(id)sender {
+    if (_result != 0) {
+        [self numberWithOperation:_operation];
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", _result];
+        _display = [self.resultLabel.text floatValue];
+        _result = 0;
+    }
+    
+    [self numberWithOperation:Add];
+}
+
+- (IBAction)equal:(id)sender {
+    [self numberWithOperation:_operation];
+    self.resultLabel.text = [NSString stringWithFormat:@"%.2f", _result];
+    _display = [self.resultLabel.text floatValue];
+    _result = 0;
+}
+
+- (IBAction)dot:(id)sender {
+    _isDecimal = TRUE;
+    NSRange range = [self.resultLabel.text rangeOfString:@"."];
+    if (range.location == NSNotFound) {
+        self.resultLabel.text = [self.resultLabel.text stringByAppendingString:@"."];
+    }
+}
+
+- (IBAction)seven:(id)sender {
+    [self setResultWithNumber:7];
+}
+
+- (IBAction)eight:(id)sender {
+    [self setResultWithNumber:8];
+}
+
+- (IBAction)nine:(id)sender {
+    [self setResultWithNumber:9];
+}
+
+- (IBAction)six:(id)sender {
+    [self setResultWithNumber:6];
+}
+
+- (IBAction)five:(id)sender {
+    [self setResultWithNumber:5];
+}
+
+- (IBAction)four:(id)sender {
+    [self setResultWithNumber:4];
+}
+
+- (IBAction)three:(id)sender {
+    [self setResultWithNumber:3];
+}
+
+- (IBAction)two:(id)sender {
+    [self setResultWithNumber:2];
+}
+
+- (IBAction)one:(id)sender {
+    [self setResultWithNumber:1];
+}
+
+- (IBAction)zero:(id)sender {
+    [self setResultWithNumber:0];
+}
+
+- (void)setResultWithNumber:(int)number {
+    if (_isDecimal) {
+        _display *= 10;
+        _display += number;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.0f", _display];
+    } else {
+        self.resultLabel.text = _display == 0 ? [NSString stringWithFormat:@"%d", number] : ([self.resultLabel.text stringByAppendingString:[NSString stringWithFormat:@"%d", number]]);
+    }
+    
+    _display = [self.resultLabel.text floatValue];
+}
+
+- (void)numberWithOperation:(Operation)operation {
+    if (_result == 0) {
+        _result = _display;
+    } else {
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", _result];
+        switch (operation) {
+            case Add:
+                _result += _display;
+                break;
+            case Substract:
+                _result -= _display;
+                break;
+            case Multiply:
+                _result *= _display;
+                break;
+            case Divide:
+                _result /= _display;
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    _operation = operation;
+    _display = 0;
+}
 @end
